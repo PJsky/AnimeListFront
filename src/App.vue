6ddd3c34be633b462a32a9132d4e48eedd167798
PiotrJ <jakubowsky.piotr@gmail.com>
2020-10-23 21:43:15 +0200

    Setup vue with simple template

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+    console.log(this.msg);
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background: #eee;
+  height: 200px;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
